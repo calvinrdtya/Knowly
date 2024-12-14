abb067156fb4fae84123864a6c49f22e9bfdd319
kikuk24 <tirta.afandi24@gmail.com>
2024-12-15 02:44:36 +0700

    Add Task Management

--- app/Models/MyClass.php
+++ app/Models/MyClass.php
@@ -28,4 +28,9 @@ class MyClass extends Eloquent
     {
         return $this->hasMany(User::class, 'teacher_id');
     }
+
+    public function subjects()
+    {
+        return $this->hasMany(Subject::class);
+    }
 }
---
+++ database/migrations/2024_12_14_224416_create_assignments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('teacher_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->unsignedBigInteger('class_id');
+            $table->string('title'); 
+            $table->text('description')->nullable(); 
+            $table->dateTime('due_date'); 
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignments');
+    }
+}
